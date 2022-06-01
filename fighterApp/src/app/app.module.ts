d06fdd49edce5a983cce2a64b98782a99dcb95ac
Leo Gouchon <leo.gouchon@gmail.com>
2022-06-01 14:23:11 +0200

    add filter_map component

--- fighterApp/src/app/app.module.ts
+++ fighterApp/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { MarkerService } from 'src/services/marker.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { MapFilterComponent } from './map-filter/map-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    MapFilterComponent
   ],
   imports: [
     BrowserModule,
---
+++ fighterApp/src/app/map-filter/map-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-filter',
+  templateUrl: './map-filter.component.html',
+  styleUrls: ['./map-filter.component.css']
+})
+export class MapFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
