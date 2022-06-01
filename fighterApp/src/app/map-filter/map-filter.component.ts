fb0f3f2dcf472fb3134a187d936e6353293da525
Leo Gouchon <leo.gouchon@gmail.com>
2022-06-01 16:55:09 +0200

    form du filter fire

--- fighterApp/src/app/map-filter/map-filter.component.ts
+++ fighterApp/src/app/map-filter/map-filter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MarkerService } from 'src/services/marker.service';
 
 @Component({
   selector: 'app-map-filter',
@@ -10,27 +11,29 @@ export class MapFilterComponent implements OnInit  {
   myForm!: FormGroup;
   formattedMessage!: string;
   
-  constructor(private formBuilder: FormBuilder) {
-   }
+  constructor(private formBuilder: FormBuilder,
+              private markerService: MarkerService)
+               {}
 
   ngOnInit(): void {
     this.myForm = this.formBuilder.group({
       inputIntensityMin: new FormControl(1),
-      inputIntensityMax:new FormControl(100),
-      inputRangeMin:new FormControl(1),
-      inputRangeMax:new FormControl(100),
-      fireA:new FormControl(true),
-      fireBg:new FormControl(true),
-      fireBa:new FormControl(true),
-      fireBp:new FormControl(true),
-      fireCfg:new FormControl(true),
-      fireDm:new FormControl(true),
-      fireEe:new FormControl(true)
+      inputIntensityMax: new FormControl(100),
+      inputRangeMin:     new FormControl(1),
+      inputRangeMax:     new FormControl(100),
+      fireA:             new FormControl(true),
+      fireBg:            new FormControl(true),
+      fireBa:            new FormControl(true),
+      fireBp:            new FormControl(true),
+      fireCfg:           new FormControl(true),
+      fireDm:            new FormControl(true),
+      fireEe:            new FormControl(true)
     })
 
     this.myForm.valueChanges.subscribe(x => {
       console.log('form changed')
-      console.log(x)
+      //console.log(x)
+      this.markerService.setFilter(x);
     });
   }
 
--- fighterApp/src/services/marker.service.ts
+++ fighterApp/src/services/marker.service.ts
@@ -5,9 +5,21 @@ import * as L from 'leaflet';
   providedIn: 'root'
 })
 export class MarkerService {
-
   firesAPI: string = 'http://vps.cpe-sn.fr:8081/fire';
-
+  filter = {
+      "inputIntensityMin": 1,
+      "inputIntensityMax": 100,
+      "inputRangeMin":     1,
+      "inputRangeMax":     100,
+      "fireA":             true,
+      "fireBg":            true,
+      "fireBa":            true,
+      "fireBp":            true,
+      "fireCfg":           true,
+      "fireDm":            true,
+      "fireEe":            true
+  }
+  
   constructor() { }
 
   //Pas certains du L.Map (le m en majuscule)
@@ -17,29 +29,48 @@ export class MarkerService {
                     };
       fetch(this.firesAPI, context)
         .then(response => response.json())
-          .then(response => callback(response, map))
+          .then(response => callback(response, map, this.filter))
           .catch(error => err_callback(error));     
   }
 
+  setFilter(x:any) {
+    this.filter = x;
+    console.log(this.filter)
   }
-  function callback(response:any, map: L.Map){
+
+  }
+  function callback(response:any, map: L.Map, filter:any){
     var fireIcon = L.icon({
       iconUrl: '../assets/fireIcon.png',
       iconSize: [40, 40], // size of the icon
       popupAnchor: [0,-15]
       });
-
+    
+      const layerFires = L.layerGroup();
+      map.addLayer(layerFires)
+      layerFires.clearLayers()
+      
     for(let id in response){
       const lat = response[id].lat;
       const lon = response[id].lon;
-      const marker = L.marker([lat, lon], {icon: fireIcon});
+      const intensity = response[id].intensity;
+      const range = response[id].range;
+      const type = response[id].type;
 
-      marker.addTo(map);
-      marker.bindPopup("<h1>Feu " + response[id].id + "</h1> </br> <h2> Type : </h2>" 
+      if (intensity <= filter.inputIntensityMax && intensity >= filter.inputIntensityMin
+        && range <= filter.inputRangeMax && range >= filter.inputRangeMin) {
+          //if (filter.$type){
+            const marker = L.marker([lat, lon], {icon: fireIcon});
+            marker.addTo(layerFires);
+            marker.bindPopup("<h1>Feu " + response[id].id + "</h1> </br> <h2> Type : </h2>" 
                        + response[id].type + "</br> <h2> Intensity : </h2>"
                        + response[id].intensity + "</br> <h2> Range : </h2>"
                        + response[id].range,
                       );
+         
+        //}
+        }
+      
     }
   }
 
