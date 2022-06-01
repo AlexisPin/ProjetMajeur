29ce28c8cda1ae41a62cea11997e3ead2b2cb254
AdrienLapalus <adri.lapalus@gmail.com>
2022-06-01 14:23:43 +0200

    Ajout de la navbar

--- fighterApp/src/app/app.module.ts
+++ fighterApp/src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { MarkerService } from 'src/services/marker.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { VehiculeComponent } from './vehicule/vehicule.component';
 
+
+const appRoutes : Routes = [
+  {path : 'map', component : MapComponent},
+  {path : '', component : MapComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    NavbarComponent,
+    VehiculeComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [MarkerService],
