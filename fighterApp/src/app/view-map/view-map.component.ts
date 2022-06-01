2afa1713077a3620089a503fa867890eaad8b85f
AdrienLapalus <adri.lapalus@gmail.com>
2022-06-01 14:29:22 +0200

    Ajout Navbar V3

---
+++ fighterApp/src/app/view-map/view-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-map',
+  templateUrl: './view-map.component.html',
+  styleUrls: ['./view-map.component.css']
+})
+export class ViewMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
