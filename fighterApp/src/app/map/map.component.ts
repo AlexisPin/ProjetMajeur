ea3d4d34c87781ae678cd064467625c202593987
Leo Gouchon <leo.gouchon@gmail.com>
2022-06-01 09:38:51 +0200

    add card component

---
+++ fighterApp/src/app/map/map.component.ts
@@ -0,0 +1,32 @@
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements AfterViewInit {
+  private map:any;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 45.750000, 4.850000 ],
+      zoom: 12
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+}
