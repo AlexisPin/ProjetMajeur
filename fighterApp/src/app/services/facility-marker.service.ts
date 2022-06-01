8f734a70797760eae2d8b365749d84736504f882
AdrienLapalus <adri.lapalus@gmail.com>
2022-06-01 17:04:35 +0200

    ajout facilityService

--- fighterApp/src/app/services/facility-marker.service.ts
+++ fighterApp/src/app/services/facility-marker.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class FacilityMarker.ServiceService {
+export class FacilityMarkerService {
 
   constructor() { }
 }
