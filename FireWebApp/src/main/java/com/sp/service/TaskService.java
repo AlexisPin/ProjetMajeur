019fb5f14eea7bf2706e84e056e43b8640873e20
Alexis Pincemin <alexis.pincemin@cpe.fr>
2022-06-02 12:14:40 +0200

    Spring

--- FireWebApp/src/main/java/com/sp/service/TaskService.java
+++ FireWebApp/src/main/java/com/sp/service/TaskService.java
@@ -2,6 +2,10 @@ package com.sp.service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -27,67 +31,68 @@ public class TaskService {
 	@Autowired
 	FacilityService faService;
 	
-	private ArrayList<VehicleDto> busyVehicules = new ArrayList<VehicleDto>();
-	private ArrayList<FireDto> onWorkFire = new ArrayList<FireDto>();
+	private ArrayList<Integer> busyVehicules = new ArrayList<Integer>();
+	private ArrayList<Integer> onWorkFire = new ArrayList<Integer>();
+	private Map<VehicleDto, FireDto> vehicleFireMap = new HashMap<VehicleDto, FireDto>();
+	
 	@Async
-	@Scheduled(fixedRate = 1000)
+	@Scheduled(initialDelay = 1000 * 30, fixedDelay=Long.MAX_VALUE)
 	public void scheduleFixedRateTask() {
 		ArrayList<VehicleDto> vehicles = vService.getOwnVehicles();
-		ArrayList<VehicleDto> availableVehicules = vehicles;
 		FireDto[] fires = fService.getFires();
 	    if(fires.length !=0) {
-	    	int distance = 1000000;
-	    	Coord interFireCoord = new Coord();
-	    	VehicleDto interVehicle = new VehicleDto();
-	    	if(!availableVehicules.isEmpty()) {		
-	    		for(VehicleDto vehicle : availableVehicules) {
+	    	int distance = 1000000000;
+	    	if(!vehicles.isEmpty()) {		
+	    		for(VehicleDto vehicle : vehicles) {
+	    			FireDto interFire = new FireDto();
 	    			Coord vCoord = new Coord(vehicle.getLon(),vehicle.getLat());
-	    			int nbFires = 0;
 	    			for(FireDto fire : fires) {
-	    				nbFires+=1;
 	    				Coord fCoord = new Coord(fire.getLon(),fire.getLat());
-	    				FireDto interFire = new FireDto();
 	    				int newDistance = GisTools.computeDistance2(vCoord, fCoord);
-	    				if(distance > newDistance && !busyVehicules.contains(vehicle) && !onWorkFire.contains(fire)) {
+	    				if(distance > newDistance) {
 	    					distance = newDistance;
-	    					interFireCoord = fCoord;
 	    					interFire = fire;
-	    					interVehicle = vehicle;
 	    				}
-	    				if(nbFires == fires.length) {
-	    					nbFires = 0;
-	    					onWorkFire.add(interFire);
-	    					intervention(interVehicle, interFireCoord);
-	    					busyVehicules.add(vehicle);
-	    				}
-	    				for(FireDto workFire : onWorkFire) {
-	    					if(!Arrays.asList(fires).contains(workFire)) {
-	    						onWorkFire.remove(workFire);
-	    						busyVehicules.remove(vehicle);
-	    					}
-	    				}
+	    			}
+	    			boolean busy = false;
+	    			for(final Entry<VehicleDto, FireDto> entry : vehicleFireMap.entrySet()) {
+	    			    final VehicleDto key =  entry.getKey();
+	    			    final FireDto value =  entry.getValue();
+
+	    			    if(vehicle.getId() == key.getId() && interFire.getId() == value.getId()) {
+	    			    	busy = true;
+	    			    }
+	    			}
+	    			if(!busy && interFire.getId() != null) {
+	    				vehicleFireMap.put(vehicle, interFire);
 	    			}
 	    		}
 	    	}
-	    }
+	    triggerEvent();
 	    checkVehiculeStatus(vehicles);
-	}   
-	@Async
-	private void intervention(VehicleDto vehicle, Coord fireCoord) {
-		Coord vCoord = new Coord(vehicle.getLat(),vehicle.getLon());
-		float liquidQuantity = vehicle.getLiquidQuantity();
-		if(liquidQuantity > 0) {
-		//vehicle.setLat(fireCoord.getLat()-fireCoord.getLat()/100000);
-		//vehicle.setLon(fireCoord.getLon()-fireCoord.getLon()/100000);
-
-		double distance = measure(vCoord, fireCoord);
-		System.out.println(distance);
-		vehicle.setLiquidQuantity(liquidQuantity - vehicle.getType().getLiquidConsumption());
-		saveChanges(vehicle);
 		}
 	}
 	
-	@Async
+	private void triggerEvent() {
+		for(final Entry<VehicleDto, FireDto> entry : vehicleFireMap.entrySet()) {
+		    final VehicleDto vehicle =  entry.getKey();
+		    final FireDto fire =  entry.getValue();
+		    
+		    //System.out.println("key: " + vehicle.getId() + " value: " + fire.getId());
+		    Coord fireCoord  = new Coord(fire.getLon(), fire.getLat());
+		    deplacement(vehicle, fireCoord);
+		}
+	}
+	
+
+	private void intervention(VehicleDto vehicle, Coord fireCoord) {
+		float liquidQuantity = vehicle.getLiquidQuantity();
+		if(liquidQuantity > 0) {
+			vehicle.setLiquidQuantity(liquidQuantity - vehicle.getType().getLiquidConsumption());
+			saveChanges(vehicle);
+		}
+	}
+	
 	private void backToFacility(VehicleDto vehicle) {
 		FacilityDto facility =  faService.getFacility(vehicle.getFacilityRefID());
 		vehicle.setLat(facility.getLat()-facility.getLat()/100000);
@@ -96,9 +101,7 @@ public class TaskService {
 		saveChanges(vehicle);
 	}
 	
-	@Async
 	private void checkVehiculeStatus(ArrayList<VehicleDto> vehicles) {
-		
 		for(VehicleDto vehicle : vehicles) {
 			float liquidQuantity = vehicle.getLiquidQuantity();
 			if(liquidQuantity <= 0) {
@@ -110,12 +113,35 @@ public class TaskService {
 	private void saveChanges(VehicleDto vehicle) {
     	vService.updateVehicle("bd4dd8f2-c28d-46ba-a342-9d9b99259a67", vehicle.getId(), vehicle);
 	}
+
 	
-	private void deplacement(VehicleDto vehicle, Coord fireCoord) {
-		
-		
+	private void deplacement(VehicleDto vehicle, Coord coordFinal) {
+		double vlat = vehicle.getLat();
+		double vlon = vehicle.getLon();
+		double dlat = coordFinal.getLat();
+		double dlon = coordFinal.getLon();
+		double distance = GisTools.computeDistance2(new Coord(vlon,vlat),new Coord(dlon,dlat));
+		System.out.println(distance);
+		if( distance > 100) {
+			if(vlat < dlat-dlat/100 || vlat > dlat+dlat/100){
+				vehicle.setLat(vlat+dlat/100000);
+			}
+			else {
+				vehicle.setLat(vlat-dlat/100000);
+			}
+			if(vlon < dlon-dlon/100 || vlon > dlon+dlon/100){
+				vehicle.setLon(vlon+dlon/100000);
+			}
+			else {
+				vehicle.setLon(vlon-dlon/100000);
+			}
+			saveChanges(vehicle);
+		}
+		else {
+			intervention(vehicle, coordFinal);
+		}
 	}
-	
+
 	private double measure(Coord vCoord, Coord fCoord){ 
 	    double R = 6378.137; // Radius of earth in KM
 	    double dLat = vCoord.getLat() * Math.PI / 180 - fCoord.getLat() * Math.PI / 180;
